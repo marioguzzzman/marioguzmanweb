0ace117c461f0efd170042b126c12af22b14e70c
Brian <lauwangtatbrian@gmail.com>
2019-05-09 18:53:04 +0800

    fix pages

--- javascript/landing.js
+++ javascript/landing.js
@@ -1,43 +1,105 @@
 // Reveal social buttons on name click
-$("#name").click(function() {
+$("#name").click(function () {
   $("#social-button").css("opacity", "1");
   $(".social-links").toggleClass('social-links-observed');
   $("#name span").toggleClass('underlined').toggleClass('selected');
 })
 
-window.addEventListener('load', function() {
-// Glitch skewing on offest
-var t1 = new TimelineMax({repeat: -1,repeatDelay: 5.3})
-t1.to(".textglitch1", 0.1, {skewX:900,ease: Power4.easeInOut}).to(".textglitch1", 0.04, {skewX:0,ease: Power4.easeInOut})
+window.addEventListener('load', function () {
+  // Glitch skewing on offest
+  var t1 = new TimelineMax({
+    repeat: -1,
+    repeatDelay: 5.3
+  })
+  t1.to(".textglitch1", 0.1, {
+    skewX: 900,
+    ease: Power4.easeInOut
+  }).to(".textglitch1", 0.04, {
+    skewX: 0,
+    ease: Power4.easeInOut
+  })
 
-var t2 = new TimelineMax({repeat: -1,repeatDelay: 5.32})
-t2.to(".textglitch2", 0.1, {skewX:100,ease: Power4.easeInOut}).to(".textglitch2", 0.04, {skewX:0,ease: Power4.easeInOut})
+  var t2 = new TimelineMax({
+    repeat: -1,
+    repeatDelay: 5.32
+  })
+  t2.to(".textglitch2", 0.1, {
+    skewX: 100,
+    ease: Power4.easeInOut
+  }).to(".textglitch2", 0.04, {
+    skewX: 0,
+    ease: Power4.easeInOut
+  })
 
-var t3 = new TimelineMax({repeat: -1,repeatDelay: 5.31})
-t3.to(".textglitch3", 0.1, {skewX:400,ease: Power4.easeInOut}).to(".textglitch3", 0.04, {skewX:0,ease: Power4.easeInOut})
+  var t3 = new TimelineMax({
+    repeat: -1,
+    repeatDelay: 5.31
+  })
+  t3.to(".textglitch3", 0.1, {
+    skewX: 400,
+    ease: Power4.easeInOut
+  }).to(".textglitch3", 0.04, {
+    skewX: 0,
+    ease: Power4.easeInOut
+  })
 
-var t4 = new TimelineMax({repeat: -1,repeatDelay: 5})
-t4.to(".snippetglitch", 0.1, {skewX:700,ease: Power4.easeInOut})
-  .to(".snippetglitch", 0.04, {skewX:0,ease: Power4.easeInOut})
-  .to(".snippetglitch", 0.02, {x:-20})
-  .to(".snippetglitch", 0.03, {x:0})
-  .add("split", 0)
-  .to(".snippetglitch", 0.02, {scaleY:1.1,ease: Power4.easeInOut})
-  .to(".snippetglitch", 0.04, {scaleY:1,ease: Power4.easeInOut})
-  .to(".snippetglitch", 0.1, {skewX:70,ease: Power4.easeInOut})
-  .to(".snippetglitch", 0.04, {skewX:0,ease: Power4.easeInOut})
-  .to(".snippetglitch", 0.04, {x:-20})
-  .to(".snippetglitch", 0.04, {x:0})
-  .add("split", 0)
-  .to(".snippetglitch", 0.01, {scaleY:.4,ease: Power4.easeInOut})
-  .to(".snippetglitch", 0.0, {scaleY:1,ease: Power4.easeInOut})
+  var t4 = new TimelineMax({
+    repeat: -1,
+    repeatDelay: 5
+  })
+  t4.to(".snippetglitch", 0.1, {
+      skewX: 700,
+      ease: Power4.easeInOut
+    })
+    .to(".snippetglitch", 0.04, {
+      skewX: 0,
+      ease: Power4.easeInOut
+    })
+    .to(".snippetglitch", 0.02, {
+      x: -20
+    })
+    .to(".snippetglitch", 0.03, {
+      x: 0
+    })
+    .add("split", 0)
+    .to(".snippetglitch", 0.02, {
+      scaleY: 1.1,
+      ease: Power4.easeInOut
+    })
+    .to(".snippetglitch", 0.04, {
+      scaleY: 1,
+      ease: Power4.easeInOut
+    })
+    .to(".snippetglitch", 0.1, {
+      skewX: 70,
+      ease: Power4.easeInOut
+    })
+    .to(".snippetglitch", 0.04, {
+      skewX: 0,
+      ease: Power4.easeInOut
+    })
+    .to(".snippetglitch", 0.04, {
+      x: -20
+    })
+    .to(".snippetglitch", 0.04, {
+      x: 0
+    })
+    .add("split", 0)
+    .to(".snippetglitch", 0.01, {
+      scaleY: .4,
+      ease: Power4.easeInOut
+    })
+    .to(".snippetglitch", 0.0, {
+      scaleY: 1,
+      ease: Power4.easeInOut
+    })
 });
 
 
 
 // Hide scroll down button on scroll
-if(window.innerWidth >= 768) {
-  $(window).scroll(function() {
+if (window.innerWidth >= 768) {
+  $(window).scroll(function () {
     if (window.scrollY > 50) {
       $("#scroll-down").fadeOut(500)
     } else {
@@ -47,9 +109,9 @@ if(window.innerWidth >= 768) {
 }
 
 // Scroll down to project on scroll down button click
-$("#scroll-down").click(function() {
+$("#scroll-down").click(function () {
   $("html, body").animate({
-    scrollTop: $( $('[data-scroll-target]')).offset().top - 75
+    scrollTop: $($('[data-scroll-target]')).offset().top - 75
   }, 1250, "swing");
 });
 
@@ -71,7 +133,12 @@ class TextScramble {
       const to = newText[i] || ""
       const start = Math.floor(Math.random() * 40)
       const end = start + Math.floor(Math.random() * 40)
-      this.queue.push({ from, to, start, end })
+      this.queue.push({
+        from,
+        to,
+        start,
+        end
+      })
     }
     cancelAnimationFrame(this.frameRequest)
     this.frame = 0
@@ -83,7 +150,13 @@ class TextScramble {
     let complete = 0
 
     for (let i = 0, n = this.queue.length; i < n; i++) {
-      let { from, to, start, end, char } = this.queue[i]
+      let {
+        from,
+        to,
+        start,
+        end,
+        char
+      } = this.queue[i]
       if (this.frame >= end) {
         complete++
         output += to
@@ -118,8 +191,8 @@ if (el) {
   var GlitchShuffleDelay = 500;
   var phrases = ["↓ Scroll Down  "]
   var counter = 0
-  var next = function() {
-    fx.setText(phrases[counter]).then(function() {
+  var next = function () {
+    fx.setText(phrases[counter]).then(function () {
       setTimeout(next, 10000)
     })
     counter = (counter + 1) % phrases.length
@@ -127,4 +200,3 @@ if (el) {
 
   setTimeout(next, GlitchShuffleDelay);
 }
-
